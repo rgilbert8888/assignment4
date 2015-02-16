f7ec7406eecf12969cc3a4ace17951b402992f44
Robyn <Robyn.gilb@gmail.com>
2015-02-16 18:17:54 -0500

    Styled a bit more

--- main.js
+++ main.js
@@ -14,19 +14,37 @@ function completeMessage(){
 	return firstName + " " + lastName + " is " + age +".";
 };
 
-//document.getElementById('first_name').innerHTML = firstName;
+//document.getElementById('first_name').innerHTML = firstName;	//this is wrong
 
-function setText(id,text){
+
+r
+
+function setValue(id, text){
 	document.getElementById(id).value = text;
 }
 
+// function setText(id, text){
+// 	var el = document.getElementById(id);
+// 	if(el instanceof HTMLInputElement){
+// 		el.value = text;
+// 	}else{
+// 		el.innerHTML = text;
+// 	}
+// }
+
 function onLoad(){
-	setText("first_name", firstName);
-	setText("last_name", lastName);
-	setText("age", age);
-	setText("future_age", calculateFutureAge());
-	setText("family", family);
-	setText("message", completeMessage());
+	setValue("first_name", firstName);
+	setValue("last_name", lastName);
+	setValue("age", age);
+	setValue("future_age", calculateFutureAge());
+	setValue("family", family);
+	setValue("message", completeMessage());
+	setInnerHtml("output","I am innerHTML!!!!");
+
+	//ohai riblet,
+	//instead of what you have above, you could use the setText function i 
+	//created above (commented out) and have it figure out whether 
+	//to set innerHTML or value based on type of element
 }
 
 
