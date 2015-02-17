f31fb857dbcb0a511ba61c2d8222a402ead5d666
Robyn <Robyn.gilb@gmail.com>
2015-02-17 12:37:29 -0500

    Table almost complete

--- main.js
+++ main.js
@@ -2,23 +2,44 @@
 var firstName= "Robyn";
 var lastName= "Gilbert";
 var age=28;
+
 var futureAge = age + 20;
-
-
+// need to .sort() first for the following to work...
+var family = ["Mitch", "Cindi", "Corey", "Sammy the Cat"];
+family = family.sort();
+var familyReversed = family.slice().reverse();
 
 var setText = function (id,text){  
-  // if no text, then text should be empty //
+  // if not text, then text should be empty 
   if(!text) text = '';
   document.getElementById(id).innerHTML= text;
-}
+};
+
+
+
 var myFunction= function(){
-  			setText('first_name',firstName);
+  setText('first_name',firstName);
   setText('last_name',lastName);
   setText('age',age);
+  setText('future_age', futureAge);
+  setText('family', family);
+  setText('reverse_family', familyReversed);
+  setText('message',firstName + " " + lastName + " is " + age + ".");
+  
+  if(age>30){
+	setText("message2", firstName + " is old.");
+  } else{
+	setText("message2", firstName + " is not yet 30.");
+	}
 };
 
 var reset = function(){
   setText('first_name');
   setText('last_name');
   setText('age');
+  setText('future_age');
+  setText('family');
+  setText('reverse_family');
+  setText('message');
+  setText('message2');
 };
