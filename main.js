b3f16498fae1a2e95e5d68e67aad0d29593d1a6d
Robyn <Robyn.gilb@gmail.com>
2015-02-16 19:28:46 -0500

    initial commit using a table

--- main.js
+++ main.js
@@ -1,81 +1,24 @@
 
+var firstName= "Robyn";
+var lastName= "Gilbert";
+var age=28;
+var futureAge = age + 20;
 
 
 
-var firstName = "robyn";
-var lastName = "gilbert";
-var age = 28;
-var family = ["cindi", "mitch", "corey", "lisa", "brett"];
-
-function calculateFutureAge(){
-	return age + 20;
-};
-function completeMessage(){
-	return firstName + " " + lastName + " is " + age +".";
-};
-
-//document.getElementById('first_name').innerHTML = firstName;	//this is wrong
-
-
-r
-
-function setValue(id, text){
-	document.getElementById(id).value = text;
+var setText = function (id,text){  
+  // if no text, then text should be empty //
+  if(!text) text = '';
+  document.getElementById(id).innerHTML= text;
 }
+var myFunction= function(){
+  			setText('first_name',firstName);
+  setText('last_name',lastName);
+  setText('age',age);
+};
 
-// function setText(id, text){
-// 	var el = document.getElementById(id);
-// 	if(el instanceof HTMLInputElement){
-// 		el.value = text;
-// 	}else{
-// 		el.innerHTML = text;
-// 	}
-// }
-
-function onLoad(){
-	setValue("first_name", firstName);
-	setValue("last_name", lastName);
-	setValue("age", age);
-	setValue("future_age", calculateFutureAge());
-	setValue("family", family);
-	setValue("message", completeMessage());
-	setInnerHtml("output","I am innerHTML!!!!");
-
-	//ohai riblet,
-	//instead of what you have above, you could use the setText function i 
-	//created above (commented out) and have it figure out whether 
-	//to set innerHTML or value based on type of element
-}
-
-
-// other options:
-
-// for(var i in family){
-// 		document.getElementById('family').value += family[i] + ", ";
-// 	}
-
-
-// <button id="thing" onClick="onLoad">Click Me</button>
-
-
-// // var fill = function(){
-// // 	document.getElementBy("first_name").innerHTML = firstName;
-// // 	document.getElementBy("last_name").innerHTML = lastName;
-// // }
-
-
-
-
-// //   function simulateLogin(userName)
-// //         {
-// //             var firstName = document.getElementById("first_name");
-// //             firstName.value = userName;
-// //             var goButton = document.getElementById("go");
-// //             goButton.click();
-// //         }
-
-
-//     	document.getElementById('first_name').innerHTML = firstName;
-// };
-// myFunction();
-
+var reset = function(){
+  setText('first_name');
+  setText('last_name');
+  setText('age');
+};
